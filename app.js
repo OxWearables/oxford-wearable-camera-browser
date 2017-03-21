b46ce94fbf039d20eee94e9318222f18ee36a575
supersational <svenh91@gmail.com>
2017-03-21 12:25:46 +0000

    images can be added without restarting server

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.set('port', 3000);
 String.prototype.last = Array.prototype.last = function(){
     return this[this.length - 1];
 };
+Array.prototype.extend = function(array) {
+    for (var i = 0, len = array.length; i < len; ++i) {
+        this.push(array[i]);
+    };    
+}
 
 // for resizing
 var img_sizes = {
@@ -29,16 +34,15 @@ var img_sizes = {
 	medium: [864,645],
 	full: [2592,1936]
 }
+
 class Db {
 
 	constructor() {
-		this.cache = {};
-		this.participant = [];
+		// this.cache = {};
+		this.participants = [];
 	}
 	load() {
-		var p_dir = 'images'
-		var annotation_dir = 'annotation'
-		Promise.all([fs.mkdirAsync(p_dir),fs.mkdirAsync(annotation_dir)])
+		Promise.all([fs.mkdirAsync('images'),fs.mkdirAsync('annotation')])
 		.catch({code:"EEXIST"}, (e) =>{
 			console.log("folder exists:", e.path	)
 		})
@@ -46,19 +50,19 @@ class Db {
 			console.log("folder creation done")
 		})
 		.then( () => {
-			return fs.readdirAsync(p_dir).map(
+			return fs.readdirAsync('images').map(
 				(p_folder) => {
 					var sizes = {}
 					console.log("participant folder:", p_folder);
 
 					
-					var dir = path.join(p_dir, p_folder)
+					var dir = path.join('images', p_folder)
 
 					if (fs.lstatSync(dir).isDirectory()) {
 						// ensure corresponding annotation directory also exists
-						fs.statAsync(path.join(annotation_dir, p_folder)).catch( {code:'ENOENT'}, (e) => {
-							console.log("creating annotatioin dir:", path.join(annotation_dir, p_folder))
-							fs.mkdir(path.join(annotation_dir, p_folder));
+						fs.statAsync(path.join('annotation', p_folder)).catch( {code:'ENOENT'}, (e) => {
+							console.log("creating annotatioin dir:", path.join('annotation', p_folder))
+							fs.mkdir(path.join('annotation', p_folder));
 						})
 						// ensure all size directories exist
 						return Promise.each(
@@ -90,7 +94,7 @@ class Db {
 					}).then(() => {
 
 						console.log('is dir')
-						this.participant.push({
+						this.participants.push({
 							name:p_folder,
 							dir:dir,
 							sizes:sizes
@@ -103,53 +107,139 @@ class Db {
 			}).all()
 		}).then(() => {
 			console.log('done reading')
-			console.log(this.participant)
+			console.log(this.participants)
 		}).then(() => {
 			console.log('ensuring all sizes exist')
-			Promise.each(this.participant, (p) => {
+			var queue = [];
+			Promise.each(this.participants, (p) => {
 
-				p.sizes.full.forEach((f) => {
-					process_full(p.name, p_dir, f)
+				console.log("all", p.sizes.full.length)
+				return Promise.map(p.sizes.full,
+				// p.sizes.full.forEach(
+					(f) => {
+						// console.log(f)
+					return process_full(p.name, f, Image_processor.queue);
+					
 				})
-
+			}).then( ()=>{
+				console.log(Image_processor.queue.length +  " images in resizing queue")
 			})
-			console.log('all sizes should be created')
 		})
 	}
 }
 
-function process_full(p_name, p_dir, f) {
-	var f_medium = path.join(p_dir, p_name, 'medium',f)
+var Image_processor = {
+	queue: [],
+	busy:false,
+	process_next: function() {
+		var queue_item = Image_processor.queue.pop();
+		console.log(queue_item)
+		Image_processor.process_image(queue_item[0],queue_item[1],queue_item[2])
+	},
+	process_image: function(size, f, p_name)  {
+		Image_processor.busy = true;
+		jimp.read(path.join('images', p_name, 'full',f))
+			.then((img) => {
+			// if (err) throw err;
+			img.resize(img_sizes[size][0],img_sizes[size][1])
+				.write(path.join('images', p_name, size,f))
+		}).then(()=>{
+			if (Image_processor.queue.length>0) Image_processor.process_next();
+			else {
+				Image_processor.busy = false;
+				console.log('done')
+				console.log('all sizes should be created')
+			}
+		})
+	}
+}
+setInterval(() => {
+	if (!Image_processor.busy && Image_processor.queue.length>0) {
+		Image_processor.process_next()
+	}
+}, 1000)
+
+watch.watchTree(__dirname + "/images", {'interval':1}, function (f, curr, prev) {
+	if (typeof f == "object" && prev === null && curr === null) {
+	  // Finished walking the tree
+	} else if (prev === null) {
+	  console.log('new file added:', f)
+	  imagesModified('added',f)
+	} else if (curr.nlink === 0) {
+	  console.log('file removed:', f)
+	  imagesModified('deleted',f)
+	  // f was removed
+	} else {
+	  console.log('file modified:', f)
+	  // f was changed
+	}
+})
+function imagesModified(state, f) {
+	var rel_f = path.relative(path.join(__dirname,'images'), f);
+	console.log(state, rel_f)
+	var rel_dir = path.dirname(rel_f);
+	console.log(rel_dir)
+	if (rel_dir!=='.' && rel_dir.length>0) {
+		var p_name = rel_dir.split(path.sep, 1)[0];
+		if (p_name in db.participants) {
+			console.log("good pariticipant", p_name)
+			var size = rel_dir.slice(p_name.length+1);
+			if (size=='full') {
+				if (state='added') {
+					// console.log(db.participants[p_name].sizes.full)
+					var filename = path.basename(f);
+					db.participants[p_name].sizes.full.push(filename)
+					process_full(p_name, filename, Image_processor.queue)
+				}
+			}
+			// console.log(rel_dir)
+			// console.log(rel_dir.slice(p_name.length+1))
+		}
+		else console.log("bad participant name", p_name);
+	}
+	
+	// console.log(path.parse(rel_f))
+	// if (rel_f.startsWith(path.join('images')) )
+}
+function process_full(p_name, f, queue) {
+	// var queue = [];
+	var f_medium = path.join('images', p_name, 'medium',f)
+	var f_thumbnail = path.join('images', p_name, 'thumbnail',f)
 	// console.log(f_medium)
-	fs.stat(f_medium, (err, stat) => {
+	return Promise.all([
+	fs.statAsync(f_medium).catch({code:'ENOENT'}, (err) => {
+		// .then( (err, stat) => {
 		if (err!==null && err.code == 'ENOENT') {
 			console.log(p_name, 'need to create medium size for:', f);
-			// sharp(path.join(p_dir, p.name, 'full',f))
+			// sharp(path.join('images', p.name, 'full',f))
 			// 	.resize(img_sizes['medium'][0],img_sizes['medium'][1])
 			// 	.toFile(f_medium)
-
-			jimp.read(path.join(p_dir, p_name, 'full',f), function(err,img){
+			queue.push(['medium', f, p_name])
+			if (0) jimp.read(path.join('images', p_name, 'full',f), function(err,img){
 				if (err) throw err;
 				img.resize(img_sizes['medium'][0],img_sizes['medium'][1])
 					.write(f_medium)
 			});
 			
 		}
-	})
-	var f_thumbnail = path.join(p_dir, p_name, 'thumbnail',f)
-	fs.stat(f_thumbnail, (err, stat) => {
+	}),
+	fs.statAsync(f_thumbnail).catch({code:'ENOENT'}, (err) => {
+		// .then( (err, stat) => {
 		if (err!==null && err.code == 'ENOENT') {
 			console.log(p_name, 'need to create thumbnail size for:', f);
-			// sharp(path.join(p_dir, p_name, 'full',f))
+			// sharp(path.join('images', p_name, 'full',f))
 			// 	.resize(img_sizes['thumbnail'][0],img_sizes['thumbnail'][1])
 			// 	.toFile(f_thumbnail)
-			jimp.read(path.join(p_dir, p_name, 'full',f), function(err,img){
+			queue.push(['thumbnail', f, p_name])
+			if (0) jimp.read(path.join('images', p_name, 'full',f), function(err,img){
 				if (err) throw err;
 				img.resize(img_sizes['thumbnail'][0],img_sizes['thumbnail'][1])
 					.write(f_thumbnail)
 			});
 		}
 	})
+	])
+	// return queue;
 }
 
 var db = new Db()
@@ -164,8 +254,8 @@ app.get('/favicon.ico', (req, res) => {
   res.sendFile(__dirname+'/static/favicon.ico')
 })
 app.get('/', function (req, res) {
-	if (db.participant.length>0) {
-	  res.send('Hello World!'+db.participant.map( p => '<br><a href="participant/'+p.name+'">'+p.name+' - '+p.sizes.full.length + ' images </a>').reduce((a, b) => a+b));
+	if (db.participants.length>0) {
+	  res.send('<h1>Participant list:</h1><br>'+db.participants.map( p => '<br><a href="participant/'+p.name+'">'+p.name+' - '+p.sizes.full.length + ' images </a>').reduce((a, b) => a+b));
 	} else {
 		res.send('Hello World!<br>no participants exist, add images to the images folder to create');
 	}
@@ -174,7 +264,7 @@ app.get('/', function (req, res) {
 app.get('/participant/:pName', function (req, res) {
 	var name = req.params.pName;
 
-	var p = db.participant.find(i => i.name===name);
+	var p = db.participants.find(i => i.name===name);
 
 	if (p===undefined) {
 		res.send("no user with name: " + name + " - " + p);
@@ -224,7 +314,7 @@ app.post('/participant/:pName/images/', function (req, res) {
 	console.log( req.query )
 	var name = req.params.pName;
 
-	var p = db.participant.find(i => i.name===name);
+	var p = db.participants.find(i => i.name===name);
 	if (p===undefined) {
 		res.status(400);
 		res.send('invalid participant`');
@@ -304,7 +394,7 @@ function parse_schema(fn) {
 var server = http.createServer(app)
 
 var reloadServer = reload(server, app);
-watch.watchTree(__dirname + "/static", {'interval':0.01}, function (f, curr, prev) {
+watch.watchTree(__dirname + "/static", {'interval':0.1}, function (f, curr, prev) {
     // Fire server-side reload event
     reloadServer.reload();
 });
