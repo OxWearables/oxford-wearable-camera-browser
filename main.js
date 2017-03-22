fb7364135af89768d05e12d91efe63d054d1e2ab
Sven <svenh91@gmail.com>
2017-03-22 11:24:03 +0000

    hot reloading code changes

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+require('electron-reload')(__dirname);
 
 const path = require('path')
 const url = require('url')
